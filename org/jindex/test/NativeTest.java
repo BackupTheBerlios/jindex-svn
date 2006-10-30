931b8f1a23a6042fb3965f8e2db703fc72a6e8e9
sorenmat <sorenmat@055c6e4a-b420-0410-b38a-c5d579f48200>
2006-10-30 08:52:18 +0000

    ""

--- org/jindex/test/NativeTest.java
+++ org/jindex/test/NativeTest.java
@@ -6,14 +6,26 @@ import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
 public class NativeTest extends TestCase {
+    /**
+     * 
+     * @param name 
+     */
 	public NativeTest(String name) {
 		super(name);
 	}
 
+    /**
+     * 
+     * @param args 
+     */
 	public static void main(String[] args) {
 		TestRunner.runAndWait(new TestSuite(NativeTest.class));
 	}
 
+    /**
+     * 
+     * @throws java.lang.Exception 
+     */
 	public void testGetName() throws Exception {
 		String mimetype = libJIndex.getMimeType("/etc/passwd");
 		assertEquals("text/plain", mimetype);
